78b193e38f5187035af7803fd6e179b42fa3e979
Paweł Janecki <33122103+PawelJa@users.noreply.github.com>
2018-03-21 16:00:51 +0100

    joins

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -5,4 +5,4 @@ SELECT * FROM Stanowiska
 --SELECT Nazwisko, Stanowisko, ID_Oddz, Oddzialy.Nazwa FROM Pracownicy INNER JOIN Oddzialy ON Pracownicy.ID_Oddz=Oddzialy.ID ORDER BY Nazwisko
 --SELECT Nazwisko, Stanowisko, ID_Oddz, Oddzialy.Nazwa FROM Pracownicy INNER JOIN Oddzialy ON Pracownicy.ID_Oddz=Oddzialy.ID WHERE Oddzialy.Nazwa LIKE 'WARSZAWA' ORDER BY Nazwisko
 --SELECT Nazwisko, Oddzialy.Nazwa, Oddzialy.Adres, Stanowisko, Placa_pod FROM Pracownicy INNER JOIN Oddzialy ON Pracownicy.ID_Oddz=Oddzialy.ID WHERE Placa_pod>2500 ORDER BY Placa_pod
---SELECT Nazwisko, Stanowisko, Placa_pod, Stanowiska.Placa_min AS Placa_min, Stanowiska.Placa_max AS Placa_max FROM Pracownicy INNER JOIN Stanowiska ON Pracownicy.Stanowisko=Stanowiska.Stanowisko
+--SELECT Nazwisko, Stanowiska.Stanowisko, Placa_pod, Stanowiska.Placa_min AS Placa_min, Stanowiska.Placa_max AS Placa_max FROM Pracownicy INNER JOIN Stanowiska ON Pracownicy.Stanowisko=Stanowiska.Stanowisko ORDER BY Nazwisko
