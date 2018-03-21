443b5de2e2eca7a4a65a7a9cbb74739025b89aa2
PawelJa <janecki.paw@gmail.com>
2018-03-21 10:26:46 +0100

    row functions

---
+++ SQLQuery2.sql
@@ -0,0 +1,4 @@
+SELECT * FROM Pracownicy
+
+--SELECT Nazwisko, CONCAT (LEFT(Stanowisko, 2), ID) AS Kod FROM Pracownicy
+--SELECT Nazwisko, REPLACE(REPLACE(REPLACE(Nazwisko, 'K', 'X'), 'L','X'), 'M', 'X') AS Wojna FROM Pracownicy
